4bc41f301a5a803dd28e8d7dd1cd9fe57817983b
daniyalv1851 <39700893+daniyalv1851@users.noreply.github.com>
2019-03-22 18:55:10 +0000

    Added inventory ui, how do I position stuff????

--- 20.3.19/Assets/Scripts/PlayerController.cs
+++ 20.3.19/Assets/Scripts/PlayerController.cs
@@ -23,8 +23,8 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        /*Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;*/
         
     }
     // Update is called once per frame
---
+++ 20.3.19/Assets/Scripts/UIScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float targetWidth;
+    public float targetHeight;
+    private Vector2 centre;
+
+    void Start()
+    {
+        RectTransform rt = (RectTransform)gameObject.transform;
+        centre = new Vector2(Screen.width - rt.rect.width, Screen.height - rt.rect.height);
+        
+        transform.position = centre;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
