6d95ff721942d346817a306f677a05a73e3a8825
daniyalv1851 <39700893+daniyalv1851@users.noreply.github.com>
2019-03-27 18:34:57 +0000

    began to implement save system
    things are going to get very tough, however, because we have to find a way to serialize the tilemap which is a pain

---
+++ 20.3.19/Assets/Scripts/SaveData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData
+{
+    public float[] playerPosition;
+    public int health;
+    public string[] items;
+    public string[] tiles;
+
+    public SaveData (PlayerController player, PlayerHealth healthControl, InventoryUI inventory)
+    {
+        health = healthControl.health;
+        playerPosition = new float[3];
+        playerPosition[0] = player.transform.position.x;
+        playerPosition[1] = player.transform.position.y;
+        playerPosition[2] = player.transform.position.z;
+
+    }
+}
